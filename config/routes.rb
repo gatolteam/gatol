5b25924977e183e58651f2c7a6aad1b47bd0c46a
jlee257 <jlee257@berkeley.edu>
2015-10-23 01:32:07 -0700

    going back to sqlite3

--- config/routes.rb
+++ config/routes.rb
@@ -53,15 +53,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  
-
-  ### Api definition goes here ###
-
-  namespace :api, defaults: { format: :json } do
-
-    ### list our resources here ###
-
-
-  end
 end
