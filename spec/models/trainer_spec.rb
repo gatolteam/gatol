5b5ed19a0f015909fb422c9cae8e438e51fae8d9
jlee257 <jlee257@berkeley.edu>
2015-10-23 03:34:35 -0700

    add test for model trainer

--- spec/models/trainer_spec.rb
+++ spec/models/trainer_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Trainer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:trainer) }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+
+  it { should respond_to(:password) }
+
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+
+
+
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
