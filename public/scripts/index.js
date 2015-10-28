964e8a387298ffc11c404278d7a2a79c277d191a
saurabhmitra <saurabh.mitra@berkeley.edu>
2015-10-28 16:14:56 -0700

    Modified question tests.

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,16 @@
 questions = [new Question("What is two plus two?", "4", ["1", "2", "3", "potato"]),
 			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
+			 new Question("The square root of 1600 is 40.", "true", ["false"]),
 			 new Question("Which of these is not a color?", "cheese stick", ["red", "orange", "yellow", "green", "blue", "purple"])];
 
 var game = new Game(questions);
