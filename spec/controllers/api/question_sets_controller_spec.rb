6f46570c73b46fd242d27c96a940aa2f4e0f7c42
Olivia Chen <odevchen@gmail.com>
2015-11-03 17:17:33 -0800

    Modify QS factories

--- spec/controllers/api/question_sets_controller_spec.rb
+++ spec/controllers/api/question_sets_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::QuestionSetsController, type: :controller do
 		before(:each) do
 			@user = FactoryGirl.create(:trainer, id: 1234)
 			request.headers['Authorization'] =  @user.auth_token
-			FactoryGirl.create(:question_set)
+			FactoryGirl.create(:question_set_varied)
 		end
 		it "gets all the QuestionSets belonging to a User" do
 			get :index
--- spec/factories/question_sets.rb
+++ spec/factories/question_sets.rb
@@ -3,9 +3,28 @@ FactoryGirl.define do
   	id 5
     setname "simpleset"
     trainer_id 1234
-	after(:create) do |set|
-	    set.questions << create(:question, question_set: set)
+
+
+    # factory will generate a set with 2 of the same question by default
+    # use `create(:question_set_repeat, question_count: 15)`,
+    # 		replacing 15 with the desired question repeat count.
+    factory :question_set_repeat do
+    	transient do
+    		question_count 2
+    	end
+      after(:create) do |set, evaluator|
+        create_list(:question, evaluator.question_count, question_set: set)
+      end
+  	end
+
+  	# factory will generate a set with 2 different questions
+  	factory :question_set_varied do
+      after(:create) do |set|
+        set.questions << create(:question, question_set: set)
 	    set.questions << create(:question_color, question_set: set)
-	end
+      end
+  	end
+
+
   end
 end
