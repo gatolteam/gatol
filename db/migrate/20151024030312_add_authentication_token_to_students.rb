c35dc18fd0dcf6a43effebed00bdb7d4d97b7144
jlee257 <jlee257@berkeley.edu>
2015-11-04 10:55:20 -0800

    make it better

---
+++ db/migrate/20151024030312_add_authentication_token_to_students.rb
@@ -0,0 +1,7 @@
+class AddAuthenticationTokenToStudents < ActiveRecord::Migration
+  def change
+    add_column :students, :auth_token, :string, default: ""
+    add_column :students, :confirmed,	default: false
+    add_index :students, :auth_token, unique: true
+  end
+end
