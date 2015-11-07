8a7b5bbc9ce91ed6600d79ef0ee47bd2894f9a3e
jlee257 <jlee257@berkeley.edu>
2015-11-06 16:08:38 -0800

    fixed username

--- db/migrate/20151024005149_add_authentication_token_to_trainers.rb
+++ db/migrate/20151024005149_add_authentication_token_to_trainers.rb
@@ -2,7 +2,7 @@ class AddAuthenticationTokenToTrainers < ActiveRecord::Migration
   def change
     add_column	:trainers, :auth_token, :string, default: ""
     add_column 	:trainers, :confirmed, :boolean, default: false
-    add_column  :trainers, :username, :string, null: false
+    add_column  :trainers, :username, :string, defaul: ""
     add_index	:trainers, :auth_token, unique: true
 
   end
