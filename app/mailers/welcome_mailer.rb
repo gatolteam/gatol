deb9bf2fa06150c62aba2f15cfc012c8c1780cd5
jlee257 <jlee257@berkeley.edu>
2015-11-22 18:11:08 -0800

    mail for reset password and game invitation

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -8,8 +8,25 @@ class WelcomeMailer < ApplicationMailer
 		if @user.is_trainer?
 			@type = 'trainers'
 		end
-		mail(to: @user.email, subject: 'Verification Email')
+		mail(to: @user.email, subject: 'Game-a-thon of Learning: Verification Email')
 		
+	end
+
+
+	def reset_password_email(user, pw)
+
+		@user = user
+		@new_pass = pw
+		mail(to: @user.email, subject: 'Game-a-thon of Learning: New Password')
+
+	end
+
+
+
+	def game_invitation_email(email_address, user, game)
+		@user = user
+		@game = game
+		mail(to: email_address, subject: 'Game-a-thon of Learning: Game Invitation')
 	end
 
 
