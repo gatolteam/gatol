a7e99e2cae8385a2ca8ce432029b6b930b561e63
jlee257 <jlee257@berkeley.edu>
2015-11-25 17:11:28 -0800

    removed public error pages

--- python/api_tests.py
+++ python/api_tests.py
@@ -11,7 +11,7 @@ import random
 # SERVER = 'http://localhost:3000'
 # SERVER = 'https://calm-garden-9078.herokuapp.com'
 SERVER = 'https://gatol.herokuapp.com'
-TEST = ""
+TEST = "test001"
 FILEPATH = 'Book1.csv'
 TIMEOUT = 200
 PASS = 'samplePass'
